090bc4ad27bc9af9e69da6f20347ecb8bbdb348f
Joel Hart <joel.d.hart1@gmail.com>
2023-01-04 15:19:45 -0800

    added script for image generation and styling but not working

--- Random_Photo_Project/script.js
+++ Random_Photo_Project/script.js
@@ -1,1 +1,23 @@
+const container = document.querySelector(".container");
+const unsplashURL = "https://source.unsplash.com/random/";
+const rows = 10;
 
+console.log("script running");
+
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement("img");
+  img.src = `${unsplashURL}${getRandomSize()}`;
+  container.appendChild(img);
+}
+
+function getRandomSize() {
+  return `${getRandomNum()}x${getRandomNum()}}`;
+}
+
+console.log(getRandomSize());
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 10) + 300;
+}
+
+console.log(getRandomNum(), "should have a number");
